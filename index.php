61bef9516e4b3aae6c2dc50628688c44e3939e30
Daniel <Daniel@Daniel-PC>
2020-05-09 13:59:25 -0300

    Version 1

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Alimentos Saludables </title>
+    </head>
+    
+    <body>
+       
+        <header> <span class="a1">Tu pagina favorita para conocer sobre alimentos saludables </span> </header>
+        <nav>
+               <a href="#">Home</a> 
+               <a href="#">Blog</a> 
+               <a href="#">Contacto</a> 
+        </nav>
+        
+        <section>
+          
+        <h1> Alimentos Saludables y Beneficiosos para la salud </h1>
+        
+        <h2> Alimentos Funcionales </h2>
+	<p> Son todos los productos potencialmente saludables, 
+            en los que se incluye cualquier alimento o ingrediente alimenticio, 
+            que pueda proporcionar un efecto benéfico 
+            para la salud además de los nutrientes tradicionales. </p>
+        
+
+	<h2> Cebolla y Ajo </h2>
+	<p> La cebolla y el ajo, alimentos utilizados 
+            como condimentos o especias en guisos y ensaladas. 
+            Ambos tienen alto aporte de <em>flavonoides </em>, 
+            <em>compuestos polifenólicos</em> con propiedades antioxidantes y compuestos organoazufrados.
+            Su consumo esta asociado con una disminución de riesgos de enfermedad cardiovascular,
+            stress oxidativo y además poseen un efecto anticancerígeno </p>
+        <h2> Té</h2> 
+        <p> Aporta polifenoles y en forma específica flavonoides como el kemferol y quercetina, 
+            las que se correlacionan con la disminución de enfermedades crónicas no transmisibles. </p>
+        
+        <h2> Yogur </h2> 
+        <p> Alto valor nutritivo por su aporte de calcio, 
+            proteínas de buena calidad biologica, contenido de riboflavina 
+            y aporte de probióticos, que le otorgan el sello de alimento funcional. </p>
+        
+        <h2> Semilla de Linaza </h2> 
+        <p> Altas concentraciones de antioxidantes, tales como flavonoides y lignanos, 
+            ácido fítico, tocoferoles y aporta mucílagos hidrosolubles. </p>
+        
+        <h2> Probioticos </h2> 
+        <p> Microorganismos beneficiosos para el ser humano que ayudan a mantener
+            el equilibrio de la flora intestinal y ayudan al sistema inmune </p>
+        
+        </section>
+        <aside>
+            
+            <p> 
+                ¿Sabias que?<br /> 
+                El chocolate con alto porcentaje de cacao
+                contiene compuestos antoxidantes, 
+                que disminuyen el riesgo de efermedades cardiacas </p>
+        </aside>
+        
+        <footer>
+        <p>  
+            Si deseas consultar algo o dar alguna sugerencia <br />
+            Escribe al e-mail de contacto:
+            <a a href="alimentos.saludables.com"> alimentos.saludables@gmail.com </a>
+        </p>
+        </footer>
+		
+    </body>
+    
+</html>
